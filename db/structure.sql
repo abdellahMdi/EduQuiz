186ae040e0bdee2cb1ff13875d4667b090d5cd79
Abdellah <hibazekraoui23@email.com>
2026-05-13 12:24:12 +0100

    final db structure

--- db/structure.sql
+++ db/structure.sql
@@ -1,59 +1,65 @@
 CREATE DATABASE EduQuiz;
 USE EduQuiz;
 
--- 1. Table Users (Jam3a les profs o les étudiants)
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       name VARCHAR(50) NOT NULL,
-                       email VARCHAR(100) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL, -- 255 bach y9bel l'hashage dyal PHP
-                       role ENUM('teacher', 'student') NOT NULL
+CREATE TABLE `users` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(50)  NOT NULL,
+  `email` VARCHAR(100)  NOT NULL,
+  `passord` VARCHAR(255)  NOT NULL,
+  `role` ENUM('teacher', 'student') NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
--- 2. Table Quizzes
-CREATE TABLE quizzes (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         quiz_code VARCHAR(12) UNIQUE NOT NULL,
-                         title VARCHAR(50) NOT NULL,
-                         description VARCHAR(255),
-                         teacher_id INT NOT NULL,
-                         FOREIGN KEY (teacher_id) REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE `quizes` (
+  `id` INT AUTO_INCREMENT,
+  `quiz_code` VARCHAR(12) UNIQUE,
+  `title` VARCHAR(50)  NOT NULL,
+  `description` VARCHAR(255),
+  `teacher_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `quiz_code`),
+  FOREIGN KEY (`teacher_id`)
+      REFERENCES `users`(`id`)
 );
 
--- 3. Table Questions
-CREATE TABLE questions (
-                           id INT AUTO_INCREMENT PRIMARY KEY,
-                           quiz_id INT NOT NULL,
-                           question_text TEXT NOT NULL,
-                           FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+CREATE TABLE `questions` (
+  `id` INT AUTO_INCREMENT,
+  `question` TEXT NOT NULL,
+  `quiz_id` VARCHAR(12)  NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`quiz_id`)
+      REFERENCES `quizes`(`quiz_code`)
 );
 
--- 4. Table Options (Les choix de réponses : y9dro ykono 2, 4, wla 6 !)
-CREATE TABLE options (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         question_id INT NOT NULL,
-                         option_text VARCHAR(255) NOT NULL,
-                         is_correct BOOLEAN NOT NULL DEFAULT FALSE, -- BOOLEAN bach n3rfo wach had l'option hiya s7i7a
-                         FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
+CREATE TABLE `reponces` (
+  `id` INT AUTO_INCREMENT,
+  `option` VARCHAR(255),
+  `is_correct` BOOLEAN NOT NULL DEFAULT FALSE,
+  `question_id` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
--- 5. Table Attempts (L'étudiant mli kaywrej l quiz)
-CREATE TABLE attempts (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
-                          student_id INT NOT NULL,
-                          quiz_id INT NOT NULL,
-                          score DECIMAL(4,2) NULL, -- NULL f lbedya, hta ysali 3ad n3mroha
-                          FOREIGN KEY (student_id) REFERENCES users(id) ON DELETE CASCADE,
-                          FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+CREATE TABLE `studentanswers` (
+  `id` INT AUTO_INCREMENT,
+  `qestion_id` INT NOT NULL,
+  `result_id` INT NOT NULL,
+  `option_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`qestion_id`)
+      REFERENCES `questions`(`id`),
+  FOREIGN KEY (`option_id`)
+      REFERENCES `reponces`(`id`)
 );
 
--- 6. Table Student_Answers (Ach khtar l'étudiant f kol question)
-CREATE TABLE student_answers (
-                                 id INT AUTO_INCREMENT PRIMARY KEY,
-                                 attempt_id INT NOT NULL,
-                                 question_id INT NOT NULL,
-                                 option_id INT NOT NULL, -- L'ID dyal l'option li khtar
-                                 FOREIGN KEY (attempt_id) REFERENCES attempts(id) ON DELETE CASCADE,
-                                 FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
-                                 FOREIGN KEY (option_id) REFERENCES options(id) ON DELETE CASCADE
+CREATE TABLE `results` (
+  `id` INT AUTO_INCREMENT,
+  `score` DECIMAL(4,2),
+  `student_id` INT NOT NULL,
+  `quiz_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`student_id`)
+      REFERENCES `users`(`id`),
+  FOREIGN KEY (`quiz_id`)
+      REFERENCES `quizes`(`id`),
+  FOREIGN KEY (`id`)
+      REFERENCES `studentanswers`(`result_id`)
 );
