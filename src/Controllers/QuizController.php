ae27a122bb66ea14c734cd78fe9eff106fb95da5
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-05-15 11:35:59 +0100

    travail sur la logic de code php et DataBase SQL

--- src/Controllers/QuizController.php
+++ src/Controllers/QuizController.php
@@ -1,9 +1,18 @@
 <?php
 declare(strict_types=1);
 
+use services\QuizModel;
+
+require_once __DIR__ . '/../../config/Database.php';
+require_once __DIR__ . '/../services/QuizModel.php';
 
 class QuizController
 {
+    private QuizModel $quizModel;
+    public function __construct(){
+        $pdo = DB::connect();
+        $this->quizModel = new QuizModel($pdo);
+    }
 public function showEditQuiz(int $quizId){
 
 
@@ -11,6 +20,26 @@ public function showEditQuiz(int $quizId){
 }
     public function storeQuestion()
     {
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $quizId = isset($_POST['quizId']) ? (int)$_POST['quizId'] : 0;
+    $questionText = trim($_POST['question'] ?? '');
+    $options = $_POST['options'] ?? [];
+    $correctIndexes = $_POST['is_correct'] ?? [];
+}if ($quizId > 0 && !empty($questionText) && count($options) >= 2 && count($correctIndexes) > 0) {
+        $success = $this->quizModel->addQuestionWithOptions($quizId, $questionText, $options, $correctIndexes);
+        if ($success) {
+            header("Location: /chemin/vers/edit_quiz.php?id=" . $quizId . "&success=1");
+            exit();
+        }else {
+            $error = "Erreur système : la question n'a pas pu être enregistrée.";
+        }
+    } else {
+        $error = "Erreur : Veuillez entrer une question, au moins 2 options, et cocher la bonne réponse.";
+    }
+        if (isset($error)) {
+            echo "<script>alert('$error');</script>";
+            $this->showEditQuiz($quizId);
+        }
+      }
     }
 }
--- src/services/QuizModel.php
+++ src/services/QuizModel.php
@@ -2,7 +2,49 @@
 declare(strict_types=1);
 namespace services;
 
+use PDO;
+
 class QuizModel
 {
+private PDO $pdo;
+public function __construct(PDO $pdo)
+{
+    $this->pdo = $pdo;
+}
+public function createQuiz(int $teacherId , string $title , string $description ) : ?string
+{
+try {
+    $quizCode = 'QZ-'.strtoupper(substr(uniqid(),-5));
+    $sql = "INSERT INTO quizzes (quiz_code, title, description, teacher_id) VALUES (?, ?, ?, ?)";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute([$quizCode, $title, $description, $teacherId]);
+    return $quizCode;
+} catch (PDOException $e) {
+     return null;
+}
+}
+public function addQuestionWithOptions(int $quizId , string $questionText , array $options ,array $correctIndexes) : bool
+{
+    try {
+        $this->pdo->beginTransaction();
+        $sqlQuestion = "INSERT INTO questions (quiz_id, question) VALUES (?, ?)";
+        $stmtQuestion = $this->pdo->prepare($sqlQuestion);
+        $stmtQuestion->execute([$quizId, $questionText]);
 
+        $questionId = (int) $this->pdo->lastInsertId();
+        $sqlOption = "INSERT INTO reponces (question_id, option_text, is_correct) VALUES (?, ?, ?)";
+        $stmtOption = $this->pdo->prepare($sqlOption);
+        foreach ($options as $index => $text) {
+            // Kan9elbo wach had l'index kayn f liste dyal les checkboxes li tcochaw s7a7
+            $isCorrect = in_array((string)$index, $correctIndexes, true) ? 1 : 0;
+
+            $stmtOption->execute([$questionId, $text, $isCorrect]);
+            $this->pdo->commit();
+            return true;
+
+        } catch (PDOException $e) {
+             $this->pdo->rollBack();
+            return false;
+        }
+}
 }
